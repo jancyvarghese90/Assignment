780fa9ba65057d7e2214c2cf35701a9e5831f24e
jancyvarghese90 <jancyvarghese90@gmail.com>
2024-08-08 09:03:44 +0300

    modified

--- prime.js
+++ prime.js
@@ -1,21 +1,31 @@
 // Write a JavaScript function to get an array and to read the first element of an array. Check whether it is prime or not
-let array=[10,20,30,40];
+const prompt=require("prompt-sync")({sigint:true}); 
+let input = prompt("Enter a list of numbers separated by commas:");
+let array = input.split(",");
+
+console.log(array);
+function first_elemnet()
+{
+let first_number=array.shift(0);
+//console.log(first_number);
 let isprime=true;
 
-if(array[0]==1)
+if(first_number==1)
 {
     console.log('1 is neither prime nor composite number');
 }
-else if(array[0]>1){
-for (let i = 2; i<=array[0]-1; i++) {
-    if(array[0]%i==0){
+else if(first_number>1){
+for (let i = 2; i<=first_number-1; i++) {
+    if(first_number%i==0){
 isprime=false;
 break;
     }}
  if(isprime){
-    console.log(array[0],'is a primenumber');
+    console.log(first_number,'is a primenumber');
  }
  else{
-    console.log(array[0],'is a not a primenumber');
+    console.log(first_number,'is a not a primenumber');
  }
 }
+}
+first_elemnet();
